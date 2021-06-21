e8cd8f43168507afca96e0557c70fb79c9cc0406
Christopher Yashko <c.yashko@gmail.com>
2021-06-21 15:24:26 -0700

    Fix sizing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './Tiki.png';
 import './App.css';
 
+const inline = {
+  maxWidth: "100vw"
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" style={inline}  />
       </header>
     </div>
   );
